5f8965df5bfc2720c1051ba6683b5a3a6616da31
Nick Boyce <Nickboyce7@outlook.com>
2023-07-01 23:13:54 -0500

    Added border and collision detection

--- main.py
+++ main.py
@@ -7,15 +7,73 @@ Additionally, code will probably be heavily commented to encourage understanding
 '''
 
 import pygame
+import math as m
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 running = True  # Initializing pygame packages, setting screen size/resolution, creating game clock
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)  # Player's position starts as mid of screen
+class Player:
+    def __init__(self, position, size):
+        self.position = position
+        self.size = size
+
+    def get_position(self):
+        return self.position
+
+    def get_size(self):
+        return self.size
+
+
+class Enemy:
+    def __init__(self, position, size):
+        self.position = position
+        self.size = size
+
+    def get_position(self):
+        return self.position
+
+    def get_size(self):
+        return self.size
+
+
+USER_START = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+user = Player(USER_START, 20)
+enemy1 = Enemy((300, 300), 15)
+
+
+# Creating a check border function to determine if player is in bounds of screen
+def check_border(pos_x, pos_y, screen_x, screen_y):
+    if (0 < pos_x < screen_x) and (0 < pos_y < screen_y):
+        pass
+    elif pos_x < 0:
+        pos_x = 1
+    elif pos_x > screen_x:
+        pos_x = screen_x - 1
+    elif pos_y < 0:
+        pos_y = 1
+    elif pos_y > screen_y:
+        pos_y = screen_y - 1
+    return pos_x, pos_y
+
+
+def check_collision(object_one, object_two):
+
+    res = tuple(map(lambda i, j: i - j, object_two.get_position(), object_one.get_position()))
+
+    if m.sqrt(res[0]**2 + res[1]**2) < (object_one.get_size() + object_two.get_size()):
+        print("hit")
+        object_one.position = pygame.Vector2(600, 300)
+    else:
+        pass
+
+    return object_one.position, object_two.position
+
 
 while running:
     # poll for events
@@ -25,22 +83,29 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)  # Set circle to screen, choose color, then size
+    #  pygame.draw.circle(screen, "red", player_pos, 20)  # Set circle to screen, choose color, then size
+    pygame.draw.circle(screen, "red", user.position, user.size)
+
+    pygame.draw.circle(screen, "blue", enemy1.position, enemy1.size)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:  # "K_w" is "pressing the w key"
-        if player_pos.y >= 100:  # Creating a top bound that circle can't cross, values are reversed?
-            player_pos.y -= 300 * dt  # dt is the duration (somehow). The longer its held, more the position changes
-        else:
-            continue  # If bound is reached, it does not allow player to move y-coordinate up anymore
+        user.position.y -= 300 * dt  # dt is the duration (somehow). The longer its held, more the position changes
+
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        user.position.y += 300 * dt
+
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        user.position.x -= 300 * dt
+
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        user.position.x += 300 * dt
+
+    user.position.x, user.position.y = check_border(user.position.x, user.position.y, screen_width, screen_height)
+    user.position, enemy1.position = check_collision(user, enemy1)
+    # print(player_pos.x, player_pos.y)
 
     # flip() the display to put your work on screen
     pygame.display.flip()  # I don't understand why you have to flip the screen??
